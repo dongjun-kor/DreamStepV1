d1cc91ff5594a697c8a8f1e2b63d31728eebf9f7
DONG_JUN_W <63381637+dongjun-kor@users.noreply.github.com>
2023-02-13 14:25:47 +0900

    Update view.py

--- view.py
+++ view.py
@@ -1,44 +1,49 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
-import pandas as pd
+from django.http import HttpResponse
 import requests
-import json
+import pandas as pd
 
-def index(request):
-    if request.method == 'POST' and request.FILES['file']:
+def home(request):
+    if request.method == 'POST':
         api_key = request.POST.get('api_key')
-        uploaded_file = request.FILES['file']
+        file = request.FILES['file']
         fs = FileSystemStorage()
-        filename = fs.save(uploaded_file.name, uploaded_file)
+        filename = fs.save(file.name, file)
         file_url = fs.url(filename)
+        df = pd.read_excel(file_url)
 
-        xlsx_file = pd.read_excel(file_url)
         results = []
-
-        for document in xlsx_file.iloc[:, 0]:
-            data_list = {'document': document}
-            inputs_list = {'data': data_list}
-            req_list = {'Inputs': inputs_list, 'GlobalParameters': 1.0}
-
-            req_json = json.dumps(req_list)
+        for doc in df['document']:
             headers = {
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + api_key
+                'Authorization': f'Bearer {api_key}'
             }
-            response = requests.post('http://85703749-f0d5-489f-8a7f-724648002ab4.koreacentral.azurecontainer.io/score', headers=headers, data=req_json)
+
+            data = {
+                'Inputs': {
+                    'input1': {
+                        'ColumnNames': ['document'],
+                        'Values': [[doc]]
+                    }
+                },
+                'GlobalParameters': {}
+            }
+
+            response = requests.post('http://85703749-f0d5-489f-8a7f-724648002ab4.koreacentral.azurecontainer.io/score', headers=headers, json=data)
 
             if response.status_code >= 400:
-                print('The request failed with status code:', response.status_code)
-                print(response.headers)
+                return HttpResponse(f'Request failed with status code: {response.status_code}')
 
-            results.append(response.json()['Results'])
+            result = response.json()['Results']['output1']['value']['Values'][0][0]
+            results.append(result)
 
-        xlsx_file['Result'] = results
-        xlsx_file.to_excel('result.xlsx', index=False)
-        response = HttpResponse(content_type='application/vnd.ms-excel')
-        response['Content-Disposition'] = 'attachment; filename="result.xlsx"'
-        xlsx_file.to_excel(response, index=False)
+        df['Result'] = results
+
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = f'attachment; filename=result_{pd.Timestamp.now().strftime("%Y-%m-%d_%H-%M-%S")}.xlsx'
+        df.to_excel(response, index=False)
         return response
 
-    return render(request, 'index.html')
+    return render(request, 'home.html')
+
